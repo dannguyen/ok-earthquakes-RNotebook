e258190021aebaa1c808dffee58935e4b4458a6f
Dan Nguyen <dansonguyen@gmail.com>
2015-08-28 19:10:16 -0700

    data

--- myslimthemes.R
+++ myslimthemes.R
@@ -1,31 +1,40 @@
 #' @import ggplot2
 #' @import grid
 
-theme_slim_strip <- function(){
+theme_dan <- function(){
   theme_minimal() +
   theme(
-    axis.text.x = element_blank(),
-    axis.text.y = element_blank(),
+    axis.text = element_text(colour = "#555555"),
+    axis.line = element_line(size = 0.1, linetype = "solid", color = "#444444"),
     axis.title = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    panel.margin = unit(0, "cm"),
-    plot.margin = unit(c(0, 0, 0, 0), "cm"),
     legend.position = "right",
     legend.key.width = unit(0.1, 'cm'),
     legend.title = element_blank(),
-    strip.text.x = element_text(size = rel(3.0))
+    panel.background = element_blank(),
+    panel.grid.major = element_line(size = 0.3, colour = "#555555",
+      linetype = "dotted"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
   )
 }
 
-theme_slim_chart <-  function(){
-  theme_slim_strip() + theme(
-    axis.text.y = element_text(color = 'black'),
-    axis.ticks.y = element_line(size = 1),
-    axis.ticks.x = element_blank(),
-    panel.grid.major = element_line(size = 0.2, colour = "#777777",
-      linetype = "dashed")
-  )
+
+
+theme_dan_map <- function(){
+    theme_dan() + theme(
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      panel.grid.major = element_blank()
+    )
 }
 
+
+
+theme_dans_grid <- function(){
+    theme_dan() + theme(
+      plot.margin = unit(c(0, 0, 0, 0), "cm"),
+      panel.margin = unit(0, "cm"),
+      strip.text.x = element_text(size = rel(3.0))
+    )
+}
