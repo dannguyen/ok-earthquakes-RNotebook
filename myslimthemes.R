fcd9ae5acf40e4a9844130421ad271a684118a99
Dan Nguyen <dansonguyen@gmail.com>
2015-08-29 12:57:20 -0700

    labels

--- myslimthemes.R
+++ myslimthemes.R
@@ -6,6 +6,7 @@ theme_dan <- function(){
   theme(
     axis.text = element_text(colour = "#555555"),
     axis.line = element_line(size = 0.1, linetype = "solid", color = "#444444"),
+    axis.line.x = element_blank(),
     axis.title = element_blank(),
     legend.position = "right",
     legend.key.width = unit(0.1, 'cm'),
@@ -35,7 +36,7 @@ theme_dan_grid <- function(){
     theme_dan() + theme(
       plot.margin = unit(c(0, 0, 0, 0), "cm"),
       panel.margin = unit(0, "cm"),
-      strip.text.x = element_text(size = rel(3.0)),
+      strip.text.x = element_text(size = rel(2.5)),
       axis.text.x = element_blank()
     )
 }
