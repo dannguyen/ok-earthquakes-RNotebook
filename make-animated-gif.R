55c2f8e4ab83c5a9b0e9085ac5eb498168a40287
Dan Nguyen <dansonguyen@gmail.com>
2015-08-29 20:51:28 -0700

    yea

--- make-animated-gif.R
+++ make-animated-gif.R
@@ -10,6 +10,7 @@ setwd("~/Dropbox/rprojs/ok-earthquakes-Rnotebook/")
 source("./myslimthemes.R")
 theme_set(theme_dan())
 
+##############################################################
 # download map data
 fname <- "./data/cb_2014_us_state_20m.zip"
 if (!file.exists(fname)){
@@ -27,6 +28,7 @@ states_map <- us_map[!us_map$STUSPS %in%
 cg_map <- states_map[!states_map$STUSPS %in% c('AK', 'HI'), ]
 ok_map <- cg_map[cg_map$STUSPS == 'OK',]
 
+##############################################################
 # Quakes data
 fn <- './data/usgs-quakes-dump.csv'
 zname <- paste(fn, 'zip', sep = '.')
@@ -40,8 +42,12 @@ if (!file.exists(zname) || file.size(zname) < 2048){
 }
 unzip(zname, exdir="data")
 # read the data into a dataframe
+print("Reading USGS data")
 usgs_data <- read.csv(fn, stringsAsFactors = FALSE)
 
+
+##############################################################
+# Filter quake data
 quakes <- usgs_data %>% filter(year(time) >= 2005, mag >= 3.0) %>%
   filter(type == 'earthquake') %>%
   mutate(year_month = strftime(time, '%Y-%m'))
@@ -55,54 +61,120 @@ xdf <- over(sp_quakes, cg_map[, 'STUSPS'])
 cg_quakes <- cbind(sp_quakes, xdf) %>% filter(!is.na(STUSPS))
 cg_quakes <- mutate(cg_quakes, is_OK = ifelse(STUSPS == 'OK', 'OK', 'Other'))
 
+##############################################################
 # Make the map -------------------------
-
+gggmap <- ggplot() +
+  geom_polygon(data = cg_map, aes(x = long, y = lat, group = group),
+                   fill = "transparent", color = "#444444", size = 0.1) +
+  geom_polygon(data = ok_map, aes(x = long, y = lat, group = group),
+                   fill = "transparent", color = "#FF6600", size = 0.5)
 
 make_quake_map <- function(yrmth){
   this_data <- filter(cg_quakes, year_month == yrmth)
-  ggplot() +
-  geom_polygon(data = cg_map, aes(x = long, y = lat, group = group),
-                   fill = "white", color = "#444444", size = 0.1) +
-  geom_polygon(data = ok_map, aes(x = long, y = lat, group = group),
-                   fill = "white", color = "orange", size = 0.5) +
+  # make a title
+  current_date <- ymd(paste(yrmth, '02', sep = '-'))
+  this_title <- strftime(current_date, "%B %Y")
+  # make decay data
+  last_mth <- strftime(current_date %m-% months(1), '%Y-%m')
+  last_mth_data <- filter(cg_quakes, year_month == last_mth)
+  gggmap +
+    # add last month decay
+    geom_point(data = last_mth_data, aes(longitude, latitude),
+                 size = 1.3,
+                 alpha = 0.1,
+                 color = '#CC7777')  +
+    # add this month
+    geom_point(data = this_data, aes(longitude, latitude),
+                 size = 1.5,
+                 alpha = 0.5,
+                 shape = 1,
+                 color = '#990000')  +
 
-  geom_point(data = this_data, aes(longitude, latitude),
-               size = 1.5,
-               alpha = 0.5,
-               shape = 1,
-               color = 'firebrick')  +
-  coord_map("albers", lat0 = 38, latl = 42) +
-  theme_dan_map()
+
+    coord_map("albers", lat0 = 38, latl = 42) +
+    theme_dan_map() +
+    theme(
+      plot.margin=unit(c(-0.5, -1, 0, -1), "cm"),
+      axis.title = element_text(color = 'black', size = rel(0.8)),
+      axis.title.y = element_blank()) +
+      labs(x = this_title)
 }
 
 
+gggbar <- ggplot(cg_quakes, aes(factor(year_month))) +
+            geom_histogram(alpha = 0.3, aes(fill = is_OK),
+            binwidth = 1, position = "stack", width = 1.0)
 make_quake_histogram <- function(yrmth){
   this_data <- filter(cg_quakes, year_month == yrmth)
-  ggplot(cg_quakes, aes(factor(year_month))) +
-    geom_histogram(alpha = 0.3, aes(fill = is_OK),
-       binwidth = 1, position = "stack", width = 1.0) +
+  gggbar +
     geom_histogram(data = this_data, alpha = 1.0,
       aes(fill = is_OK), binwidth = 1, position = "stack", width = 1.0) +
-    scale_fill_manual(values = c("orange", "grey")) +
-    theme(axis.text.x = element_blank())
+
+    scale_y_continuous(breaks=c(0, 100)) +
+    scale_fill_manual(values = c("#FF6600", "grey")) +
+    theme(axis.text.x = element_blank(), legend.position="none",
+      axis.text.y = element_text(size = rel(0.5), hjust = 0.0, color = "#666666"),
+      panel.grid.major = element_line(size = 0.2, colour = "#777777",
+        linetype = "dotted")
+      )
 }
 
 
-make_it_so <- function(yrmth){
-  mymap <- make_quake_map(yrmth) +
-      theme(plot.margin=unit(c(-2, -3, -2, -3), "cm"))
-  mybar <- make_quake_histogram(yrmth)
+##############################################################
+# The image making function
+make_clips <- function(items){
+  lapply(items, function(ym) {
+    comp_name = paste("/tmp/movie-quakes/composite-", ym, '.png', sep = "")
+    m_name <- paste("/tmp/movie-quakes/map-", ym, '.png', sep = "")
+    ggsave(m_name, height=3, width=5, plot = make_quake_map(ym), device = 'png',
+       bg="transparent")
+    b_name <- paste("/tmp/movie-quakes/histogram-", ym, '.png', sep = "")
+    ggsave(b_name, height=2, width=5, plot = make_quake_histogram(ym),
+      device = 'png', bg="transparent")
+    print(as.character(ym))
 
-  print(grid.arrange(mymap, mybar, ncol = 2))
-}
-
-
-
-library(animation)
-yrmths = unique(cg_quakes$year_month)
-make_movie <- function(items){
-  lapply(items, function(i) {
-    make_it_so(i)
+    system(paste("convert -size 1500x1000 xc:white", comp_name))
+    system(paste("composite -geometry +0+400", b_name, comp_name, comp_name))
+    system(paste("composite -geometry +0+0", m_name, comp_name, comp_name))
   }
 )}
-saveGIF(make_movie(yrmths), interval = .1, movie.name="/tmp/quakes.gif")
+
+#######################################
+# Let's go
+print("Making a movie!")
+yrmths = unique(cg_quakes$year_month)[90:110]
+make_clips(yrmths)
+system("convert -delay 10 /tmp/movie-quakes/composite-*.png /tmp/movie-quakes/movie-quakes-OK.gif")
+
+
+# convert -size 3000x2000 xc:white composite.png
+# composite -geometry +0+800 histogram-2005-10.png composite.png composite.png
+# composite -geometry +0+100 map-2005-10.png composite.png composite.png
+
+
+
+
+# make_it_so <- function(yrmth){
+#   mymap <- make_quake_map(yrmth) +
+#         theme(plot.margin=unit(c(-2.5, -2, -2.5, -2), "cm")) +
+#         strip_backs()
+#   mybar <- make_quake_histogram(yrmth) + strip_backs()
+
+
+#   grid.arrange(mymap, mybar, ncol = 2)
+# }
+
+
+
+
+# make_movie <- function(items){
+#   lapply(items, function(ym) {
+#     fname <- paste("/tmp/mymovie-", ym, '.png', sep = "")
+#     ggsave(fname, height=5, width=10, plot = make_it_so(ym), device = 'png')
+#     print(fname)
+#   }
+# )}
+
+#make_movie(yrmths)
+
+#saveGIF(make_movie(yrmths), interval = .1, movie.name="/tmp/quakes2.gif")
