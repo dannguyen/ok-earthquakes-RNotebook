de44637d0e12871beb791c27c16c3a3b8e31846b
Dan Nguyen <dansonguyen@gmail.com>
2015-09-01 22:06:11 -0700

    all august data, cleaning up chap 4

--- scripts/fetch_quakes.py
+++ scripts/fetch_quakes.py
@@ -10,8 +10,8 @@ from os import makedirs
 from os.path import join
 import requests
 DUMP_DIR = "/tmp/usgs-quakes/"
-START_DATE = datetime(2015, 1, 1)
-END_DATE = datetime.today()
+START_DATE = datetime(1995, 1, 1)
+END_DATE = datetime(2015, 9, 1)
 BASE_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query.csv"
 TIMEFMT = '%Y-%m-%d 00:00:00'
 # make the data directory
@@ -22,11 +22,11 @@ u_params = {'orderby': 'time-asc', 'starttime': START_DATE}
 u_params['starttime'] = START_DATE.strftime(TIMEFMT)
 for dt in tspan[1:]: # skip the first date since START_DATE is already assigned
     u_params['endtime'] = dt.strftime(TIMEFMT)
-    print(u_params)
     # call the API
     resp = requests.get(BASE_URL, params = u_params)
     # Save the resulting text file
-    fn = u_params['starttime'][0:7] # just need the year-month, i.e. first 7 chars
+    # just need the year-month for the filename, i.e. first 7 chars
+    fn = u_params['starttime'][0:7]
     fname = join(DUMP_DIR, fn + '.csv')
     with open(fname, 'w') as f:
         f.write(resp.text)
